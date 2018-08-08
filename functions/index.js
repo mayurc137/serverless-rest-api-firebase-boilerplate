f04b5ec4692d0151b8d6f8b0922ac843c339ea32
Mayur More <m.mayur404@gmail.com>
2018-08-08 17:55:31 +0530

    use case for express

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,56 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({
+    origin: true
+}));
+
+// Add middleware to authenticate requests or perform additional tasks
+
+let myMiddleware = (req, res, next) => {
+    //authentication code goes here
+    next();
+}
+
+app.use(myMiddleware);
+
+
+// build multiple CRUD interfaces:
+app.get('/', (req, res) => {
+    let response = 'Hello World';
+    res.send(response);
+});
+
+app.get('/:id', (req, res) => {
+    let id = req.params.id;
+    //let response;
+    res.send(response);
+});
+
+app.post('/', (req, res) => {
+    let response = 'Hello World';
+    res.send(response);
+});
+
+
+app.put('/:id', (req, res) => {
+    let id = req.params.id;
+    //let response;
+    res.send(response);
+});
+
+app.delete('/:id', (req, res) => {
+    let id = req.params.id;
+    //let response;
+    res.send(response);
+});
+
+
+// Expose Express API as a single Cloud Function:
+exports.endpoints = functions.https.onRequest(app);
+
+
